f8c74a6046b7453939ca0334bfd6600e5eff6cbb
Hai Huang <huang195@gmail.com>
2020-05-14 17:46:37 -0400

    fix workload ip/port

--- test/test.sh
+++ test/test.sh
@@ -31,8 +31,10 @@ kubectl get vs -n bookinfo-iter8
 echo "===================================="
 echo "Generate workload"
 echo "===================================="
-IP=kubectl -n bookinfo-iter8 get services | grep productpage | awk '{print $3}'
-watch -n 0.1 'curl -H "Host: bookinfo.sample.dev" -Is "http://${GATEWAY_URL}/productpage"'&
+IP=`kubectl -n bookinfo-iter8 get services | grep productpage | awk '{print $3}'`
+PORT=`kubectl -n bookinfo-iter8 get services | grep productpage | awk '{print $5}' | awk -F/ '{print $1}'`
+echo "Service IP:port is $IP:$PORT"
+watch -n 0.1 'curl -H "Host: bookinfo.sample.dev" -Is "http://${IP}/productpage:$PORT"'&
 
 echo "===================================="
 echo "Create Iter8 Experiment"
@@ -44,5 +46,5 @@ echo "===================================="
 echo "Deploy canary version"
 echo "===================================="
 kubectl apply -n bookinfo-iter8 -f https://raw.githubusercontent.com/iter8-tools/iter8-controller/master/doc/tutorials/istio/bookinfo/reviews-v3.yaml
+kubectl wait --for=condition=ExperimentSucceeded -n bookinfo-iter8 experiments.iter8.tools reviews-v3-rollout --timeout=180s
 kubectl get experiments -n bookinfo-iter8
-
