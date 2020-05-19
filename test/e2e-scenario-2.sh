6a26801b74e774bc91804ef226224decab3f9d4b
Hai Huang <huang195@gmail.com>
2020-05-19 15:00:51 -0400

    fix success conditions

--- test/e2e-scenario-2.sh
+++ test/e2e-scenario-2.sh
@@ -30,7 +30,7 @@ echo "Test results"
 echo "===================================="
 kubectl -n bookinfo-iter8 get experiments.iter8.tools reviews-v4-rollout -o yaml
 conclusion=`kubectl -n bookinfo-iter8 get experiments.iter8.tools reviews-v4-rollout -o=jsonpath='{.status.assessment.conclusions[0]}'`
-if [ "$conclusion" != "All success criteria were not met" ]; then
+if [ "$conclusion" == "All success criteria were  met" ]; then
   echo "Experiment succeded unexpectedly!"
   exit 1
 fi
