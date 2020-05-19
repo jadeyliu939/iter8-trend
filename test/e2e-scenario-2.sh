77502954bac5bea269454e648cdbf0669f0106af
Hai Huang <huang195@gmail.com>
2020-05-19 11:28:09 -0400

    fix typo

--- test/e2e-scenario-2.sh
+++ test/e2e-scenario-2.sh
@@ -21,14 +21,14 @@ echo "Deploy canary version"
 echo "===================================="
 kubectl apply -n bookinfo-iter8 -f https://raw.githubusercontent.com/iter8-tools/iter8-controller/master/doc/tutorials/istio/bookinfo/reviews-v4.yaml
 sleep 1
-kubectl wait --for=condition=ExperimentFailed -n bookinfo-iter8 experiments.iter8.tools reviews-v3-rollout --timeout=180s
+kubectl wait --for=condition=ExperimentFailed -n bookinfo-iter8 experiments.iter8.tools reviews-v4-rollout --timeout=180s
 kubectl get experiments -n bookinfo-iter8
 
 echo ""
 echo "===================================="
 echo "Test results"
 echo "===================================="
-conclusion=`kubectl -n bookinfo-iter8 get experiments.iter8.tools reviews-v3-rollout -o=jsonpath='{.status.assessment.conclusions[0]}'`
+conclusion=`kubectl -n bookinfo-iter8 get experiments.iter8.tools reviews-v4-rollout -o=jsonpath='{.status.assessment.conclusions[0]}'`
 if [ "$conclusion" != "All success criteria were not met" ]; then
   echo "Experiment succeded unexpectedly!"
   exit 1
