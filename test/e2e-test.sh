deca4c0d3f7fe63c7fc34df22b219547075dbb47
Hai Huang <huang195@gmail.com>
2020-06-05 10:59:49 -0400

    fix relative path

--- test/e2e-test.sh
+++ test/e2e-test.sh
@@ -8,13 +8,13 @@ set -e
 
 DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1; pwd -P )"
 
-$DIR/../../iter8-controller/test/e2e-scenario-1.sh
-$DIR/../../iter8-controller/test/e2e-scenario-1-verify.sh
-$DIR/../../iter8-controller/test/e2e-scenario-2.sh
-$DIR/../../iter8-controller/test/e2e-scenario-2-verify.sh
-$DIR/../../iter8-controller/test/e2e-scenario-3.sh
-$DIR/../../iter8-controller/test/e2e-scenario-3-verify.sh
-$DIR/../../iter8-controller/test/e2e-scenario-4.sh
-$DIR/../../iter8-controller/test/e2e-scenario-4-verify.sh
-$DIR/../../iter8-controller/test/e2e-scenario-5.sh
-$DIR/../../iter8-controller/test/e2e-scenario-5-verify.sh
+$DIR/../iter8-controller/test/e2e-scenario-1.sh
+$DIR/../iter8-controller/test/e2e-scenario-1-verify.sh
+$DIR/../iter8-controller/test/e2e-scenario-2.sh
+$DIR/../iter8-controller/test/e2e-scenario-2-verify.sh
+$DIR/../iter8-controller/test/e2e-scenario-3.sh
+$DIR/../iter8-controller/test/e2e-scenario-3-verify.sh
+$DIR/../iter8-controller/test/e2e-scenario-4.sh
+$DIR/../iter8-controller/test/e2e-scenario-4-verify.sh
+$DIR/../iter8-controller/test/e2e-scenario-5.sh
+$DIR/../iter8-controller/test/e2e-scenario-5-verify.sh
