228408d2e84688f47ed554697289c3c3b0cee1b0
Hai Huang <huang195@gmail.com>
2020-06-08 09:34:02 -0400

    remove unused conditions

--- test/build-image.sh
+++ test/build-image.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-# Build the image only when this is triggered by a "Branch build", i.e., PR merge
-if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-  echo $DOCKERHUB_TOKEN | docker login -u $DOCKERHUB_USERNAME --password-stdin;
-  export IMG="iter8/iter8-trend:$TRAVIS_BRANCH";
-  echo "Building PR Docker image = $IMG";
-  make docker-build;
-  make docker-push;
-  LATEST="iter8/iter8-trend:latest";
-  docker tag $IMG $LATEST;
-  export IMG=$LATEST;
-  make docker-push;
-fi
+# Build the image only when this is triggered by a "Branch build", i.e., PR
+# merge. This is checked in .travis.yml
+echo $DOCKERHUB_TOKEN | docker login -u $DOCKERHUB_USERNAME --password-stdin;
+export IMG="iter8/iter8-trend:$TRAVIS_BRANCH";
+echo "Building PR Docker image = $IMG";
+make docker-build;
+make docker-push;
+LATEST="iter8/iter8-trend:latest";
+docker tag $IMG $LATEST;
+export IMG=$LATEST;
+make docker-push;
