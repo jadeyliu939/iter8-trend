8d5bd4ff667a98d344753edfd5ddeab3f4785d4b
Hai Huang <huang195@users.noreply.github.com>
2020-11-04 09:31:07 -0500

    fix e2e_test (#109)

--- test/e2e-test.sh
+++ test/e2e-test.sh
@@ -18,8 +18,9 @@ fi
 # Exit on error
 set -e
 
-$DIR/../iter8/test/e2e/e2e-scenario-0a.sh
-$DIR/e2e-scenario-0a-verify.sh
+$DIR/../iter8/test/e2e/e2e-canary-scenario-1.sh
+$DIR/../iter8/test/e2e/e2e-canary-scenario-2.sh
+#$DIR/e2e-scenario-0a-verify.sh
 #$DIR/../iter8/test/e2e/e2e-scenario-0b.sh
 #$DIR/e2e-scenario-0b-verify.sh
 #$DIR/../iter8/test/e2e/e2e-scenario-0c.sh
